0f06889cf1ac2b1dba82b18dfe2bfbf099703f95
msenin <mi.senin@yandex.ru>
2024-11-24 12:55:43 +0300

    отправка заявки в рэббит

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApiForRequests.Dtos;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
 
 namespace WebApiForRequests.Controllers
 {
@@ -15,7 +18,7 @@ namespace WebApiForRequests.Controllers
         }
 
         [HttpPost(Name = "SendRequest")]
-        public bool Send(RequestDto request)
+        public async Task<bool> Send(RequestDto request)
         {
             // проверка паспортных данных
 
@@ -27,16 +30,34 @@ namespace WebApiForRequests.Controllers
 
             // вернуть решение о выдаче кредита, с указанием причины
 
-
-
-            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            //{
-            //    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            //    TemperatureC = Random.Shared.Next(-20, 55),
-            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            //})
-            //.ToArray();
+            await SendToRabbit(request);
             return true;
+        }
+
+        private async Task SendToRabbit(RequestDto request)
+        {
+            ConnectionFactory factory = new ConnectionFactory();          //подключение к rabbit
+            factory.UserName = "guest";                                   //подключение к rabbit
+            factory.Password = "guest";                                   //подключение к rabbit
+            factory.VirtualHost = "/";                                    //подключение к rabbit
+            factory.HostName = "localhost";                               //подключение к rabbit
+                                                                          //подключение к rabbit
+            IConnection conn = await factory.CreateConnectionAsync();     //подключение к rabbit
+
+            string exchange = "BankExchange";
+            string routingKey = "route";
+            string queue = "CreditRequestsIn";
+
+            IChannel channel = await conn.CreateChannelAsync();                  
+            await channel.ExchangeDeclareAsync(exchange, ExchangeType.Direct);   
+            await channel.QueueDeclareAsync(queue, false, false, false, null);   
+            await channel.QueueBindAsync(queue, exchange, routingKey, null);     
+
+            byte[] messageBytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(request));
+            await channel.BasicPublishAsync(exchange, routingKey, false, messageBytes);
+
+            await channel.CloseAsync();
+            await conn.CloseAsync();
         }
     }
 }
